6e250dd68cab8add5a21fa0a78b250bd51d5d094
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2017-08-17 15:10:39 +0300

    added settings module

---
+++ starter/Runur/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'runur',
+        'USER': 'runur',
+        'PASSWORD': 'P0stGr3$P45$W0rdD',
+        'HOST': 'runur-server-dev.cszghbpq5kn0.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = '/srv/starter/static/'
