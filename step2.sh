8ae0e3b32348ef5f87d46d9e21e0b331296521fd
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2017-08-19 21:31:20 +0300

    taskdef (fixed parsing name)

--- step2.sh
+++ step2.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #Constants
-
+TASKDEF=taskdef_dev
+#TASKDEF=taskdef_prod
+#TASKDEF=taskdef_stage
 REGION=us-west-2
 REPOSITORY_NAME=hello-world
 CLUSTER=getting-started
-FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
-#NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
-NAME=docker-hello-world
+FAMILY=`jq ".family" ${TASKDEF}.json`
+NAME=`jq ".containerDefinitions[].name" ${TASKDEF}.json`
 SERVICE_NAME=${NAME}-service
 
 #Store the repositoryUri as a variable
 REPOSITORY_URI=`aws ecr describe-repositories --repository-names ${REPOSITORY_NAME} --region ${REGION} | jq .repositories[].repositoryUri | tr -d '"'`
 
 #Replace the build number and respository URI placeholders with the constants above
-sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" taskdef.json > ${NAME}-v_${BUILD_NUMBER}.json
+sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_URI};g" ${TASKDEF}.json > ${NAME}-v_${BUILD_NUMBER}.json
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
